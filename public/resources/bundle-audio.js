3e28328da096530a7863d5989bab9a59a144a777
PotHix <pothix@pothix.com>
2011-10-08 19:08:17 -0300

    Removing the deleted audio from the audio loader

--- public/resources/bundle-audio.js
+++ public/resources/bundle-audio.js
@@ -3,7 +3,6 @@
 	addAudio:[
 		["default-music",[audioserver+"intro.mp3",audioserver+"intro.ogg"],{channel:"bgmusic",loop:true}],
 		["ending",[audioserver+"ending.mp3",audioserver+"ending.ogg"],{channel:"bgmusic",loop:true}],
-		["cave",[audioserver+"cave.mp3",audioserver+"cave.ogg"],{channel:"bgmusic",loop:true}],
 
 		["default-menu-option",[audioserver+"select.mp3",audioserver+"select.ogg"],{channel:"sfx"}],
 		["default-menu-confirm",[audioserver+"start.mp3",audioserver+"start.ogg"],{channel:"sfx"}],
