4f210cab5554cfc7a4bb7bc09d79cf675b6b546a
PotHix <pothix@pothix.com>
2011-10-08 19:43:44 -0300

    Removing start and select audios

--- public/resources/bundle-audio.js
+++ public/resources/bundle-audio.js
@@ -2,8 +2,6 @@
 	// Audio resources
 	addAudio:[
 		["default-music",[audioserver+"intro.mp3",audioserver+"intro.ogg"],{channel:"bgmusic",loop:true}],
-		["default-menu-option",[audioserver+"select.mp3",audioserver+"select.ogg"],{channel:"sfx"}],
-		["default-menu-confirm",[audioserver+"start.mp3",audioserver+"start.ogg"],{channel:"sfx"}],
-		["beep",[audioserver+"beep.mp3",audioserver+"beep.ogg"],{channel:"sfx"}],
+		["beep",[audioserver+"beep.mp3",audioserver+"beep.ogg"],{channel:"sfx"}]
 	]
 }
