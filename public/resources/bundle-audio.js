d1b3fc2abb6ea727ab43ea1c6fd833c7fc4fde58
PotHix <pothix@pothix.com>
2011-10-12 01:03:51 -0300

    Changing the school audio to use the alchemist ogg song file

--- public/resources/bundle-audio.js
+++ public/resources/bundle-audio.js
@@ -2,7 +2,7 @@
 	// Audio resources
 	addAudio:[
 		["external-bgm",[audioserver+"farmland-40.mp3",audioserver+"farmland-40.ogg"],{channel:"bgmusic",loop:true}],
-		["school-bgm",[audioserver+"alchemist-40.mp3",audioserver+"standby-40.ogg"],{channel:"bgmusic",loop:true}],
+		["school-bgm",[audioserver+"alchemist-40.mp3",audioserver+"alchemist-40.ogg"],{channel:"bgmusic",loop:true}],
 		["default-music",[audioserver+"intro.mp3",audioserver+"intro.ogg"],{channel:"bgmusic",loop:true}]
 	]
 }
